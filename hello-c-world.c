6680b07d0245ca7814c7592af0740a75ca44daf3
Roman Donchenko <drstranges@gmail.com>
2016-11-07 21:57:58 +0000

    55

--- hello-c-world.c
+++ hello-c-world.c
@@ -2,8 +2,12 @@
 
 main() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     printf("Hello World! --123 1--\n"); //676767
 =======
     printf("Hello World! 1231\n"); //676767
 >>>>>>> 4
+=======
+    printf("Hello World! --1231--git\n"); //676767
+>>>>>>> 55
 }
